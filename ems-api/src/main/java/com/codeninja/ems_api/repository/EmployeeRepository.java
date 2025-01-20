77d064d20ec71a8fdedee4faed541dd4949328f7
Lin-Thet-Zaw <ammoaalinthetzaw@gmail.com>
2025-01-20 13:56:17 +0630

    First Commite

--- ems-api/src/main/java/com/codeninja/ems_api/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codeninja.ems_api.repository;
-
-import com.codeninja.ems_api.entity.EmployeeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
-}
